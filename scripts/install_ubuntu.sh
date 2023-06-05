c681ab80189206951ff1f7474995e1bc8b83ec4a
Younghoon Jeong <younghoon-j@navercorp.com>
2023-06-05 16:05:15 +0900

    feat: ubuntu install script v0.0.2

--- scripts/install_ubuntu.sh
+++ scripts/install_ubuntu.sh
@@ -1,31 +1,55 @@
 #!/bin/bash
 
-printf "Welcome to boychaboy's dotfiles installer!\n\n"
+printf "-----------------------------------------------------------------"
+printf "|      Boychaboy's dotfile installer for Ubuntu 22.04 lts       |"
+printf "-----------------------------------------------------------------"
 
 # ----------------------------------------------------------------------
 # | Clone dotfile repository                                           |
 # ----------------------------------------------------------------------
-printf "\033[33mCloning Repository...\033[0m\n"
-echo "  git@github.com:boychaboy/dotfiles.git >> ${HOME}/.dotfiles"
 git clone -b ubuntu-22.04 https://github.com/boychaboy/dotfiles.git ${HOME}/.dotfiles
 
 # ----------------------------------------------------------------------
-# | Clone dotfile repository                                           |
+# | Create symbolic links to new dotfiles                              |
 # ----------------------------------------------------------------------
-echo "Create symbolic links to new dotfiles"
 ln -sf ${HOME}/.dotfiles/.vimrc ${HOME}/.vimrc
 ln -sf ${HOME}/.dotfiles/.tmux.conf ${HOME}/.tmux.conf
+echo "Symbolic links created to new dotfiles for [.vimrc, .tmux.conf]"
 
-echo ""
-echo "Setting up .vimrc..."
 
-# Install vundle
+# ----------------------------------------------------------------------
+# | Set up .vimrc                                                      |
+# ----------------------------------------------------------------------
+#   a. Install vundle
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     echo "> Successfully installed Vundle.vim"
 fi
 
-# Install plugins
+#   b. Install plugins
 vim +PluginInstall +qall
 
+#   c. Build YCM
+apt install -y build-essential cmake vim-nox python3-dev
+apt install -y mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm
+python3 ~/.vim/bundle/YouCompleteMe/install.py --all --force-sudo
+
+
+# ----------------------------------------------------------------------
+# | Install miniconda                                                  |
+# ----------------------------------------------------------------------
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init zsh
+
+# ----------------------------------------------------------------------
+# | ETC                                                                |
+# ----------------------------------------------------------------------
+pip3 install black flake8 numpy pandas tqdm ipdb scikit-learn requests
+echo -e 'alias ca="conda activate"\nalias da="conda deactivate"\nalias envs="conda info --envs"\nalias py="python"\nalias mv="mv alias\ncp="cp -i"\nalias cat="bat"' >> ~/.zshrc
+
 source ${HOME}/.zshrc
+
+printf "\033Done!\033[0m"
